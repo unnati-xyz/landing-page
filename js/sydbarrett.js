3826c4524e078db7c903aa8abd64726f9dbd52a3
Shrayas <shrayasr@gmail.com>
2015-12-26 17:01:16 +0530

    Add albums

--- js/sydbarrett.js
+++ js/sydbarrett.js
@@ -1,23 +1,39 @@
 var songs = [
+
+  // The Piper at the Gates of Dawn
   'Astronomy Domine',
   'Lucifer Sam',
   'Interstellar Overdrive',
+
+  // A Saucerful of Secrets	
   'Set the Controls for the Heart of the Sun',
   'A Saucerful of Secrets',
+
+  // Ummagumma
   'Careful with That Axe, Eugene',
   'Several Species of Small Furry Animals Gathered Together in a Cave and Grooving with a Pict',
+
+  // Atom Heart Mother
   'Alan\'s Psychedelic Breakfast',
   'Echoes',
+
+  // The Dark Side of the Moon
   'Time',
   'The Great Gig in the Sky',
   'Us and Them',
   'Brain Damage',
   'Eclipse',
+
+  // Wish You Were Here
   'Shine On You Crazy Diamond (Parts I - V)',
   'Wish You Were Here',
   'Shine On You Crazy Diamond (Parts VI - IX)',
+
+  // Animals
   'Dogs',
   'Pigs (Three different Ones)',
+
+  // The Wall
   'The Happiest Days of Our Lives',
   'Another Brick in the Wall (Part II)',
   'Mother',
@@ -25,17 +41,25 @@ var songs = [
   'Hey You',
   'Bring the Boys Back Home',
   'Comfortably Numb',
+
+  // The Final Cut
   'The Gunner\'s Dream',
   'The Fletcher Memorial Home',
+
+  // A Momentary Lapse of Reason
   'Learning to Fly',
   'The Dogs of War',
   'One Slip',
   'Terminal Frost',
+
+  // Division Bell
   'Cluster One',
   'Marooned',
   'Wearing the Inside Out',
   'Keep Talking',
   'High Hopes',
+
+  // The Endless River
   'Talkin\' Hawkin',
   'Nervana'
 ];
