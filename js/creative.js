26feaead096a33e6317f54be41681d684c004839
Raghotham <raghotham.s@gmail.com>
2016-04-13 10:05:22 +0530

    Affix always the navbar

--- js/creative.js
+++ js/creative.js
@@ -36,11 +36,13 @@
     );
 
     // Offset for Main Navigation
+    /*
     $('#mainNav').affix({
         offset: {
             top: 100
         }
     })
+    */
 
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
